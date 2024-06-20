91a703452756e20e68a15df2a6051047eca52c82
Tina <tina.hm.gao@gmail.com>
2024-06-20 21:29:18 +1200

    step1.2.2 - remove play - inline variable

--- src/index.ts
+++ src/index.ts
@@ -15,17 +15,16 @@ const statement = (invoice: TInvoice, plays: TPlays) => {
         minimumFractionDigits: 2,
     }).format
     for (let perf of invoice.performances) {
-        const play = playFor(perf)
-        let thisAmount = amountFor(perf, play)
+        let thisAmount = amountFor(perf, playFor(perf))
 
         // add volume volumeCredits
         volumeCredits += Math.max(perf.audience - 30, 0)
         // add extra credit for every ten comedy attendees
-        if ('comedy' === play.type)
+        if ('comedy' === playFor(perf).type)
             volumeCredits += Math.floor(perf.audience / 5)
 
         // print line for this order
-        result += ` ${play.name}: ${format(thisAmount / 100)} (${
+        result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${
             perf.audience
         } seats)\n`
         totalAmount += thisAmount
